c08cf2afbd341fed2989fc120919975676a4175f
lionslair <lionslair@users.noreply.github.com>
2021-10-26 02:25:23 +0000

    Fix styling

--- src/Models/Leave.php
+++ src/Models/Leave.php
@@ -104,11 +104,11 @@ class Leave extends Model
             return __('laravel-xero-leave-translations::laravel-xero-leave.status.pending');
         }
 
-        if (!empty($this->approved_at)) {
+        if (! empty($this->approved_at)) {
             return __('laravel-xero-leave-translations::laravel-xero-leave.status.approved');
         }
 
-        if (!empty($this->declined_at)) {
+        if (! empty($this->declined_at)) {
             return __('laravel-xero-leave-translations::laravel-xero-leave.status.declined');
         }
 
@@ -117,19 +117,19 @@ class Leave extends Model
 
     public function hasXeroLeaveApplicationId(): bool
     {
-        return !empty($this->xero_leave_application_id);
+        return ! empty($this->xero_leave_application_id);
     }
 
     public function hasSyncError(): bool
     {
-        return !empty($this->xero_exception_message);
+        return ! empty($this->xero_exception_message);
     }
 
     public function shouldUpdateXero(): bool
     {
         if ($this->wasChanged(['start_date', 'end_date', 'units']) && $this->hasXeroLeaveApplicationId()) {
             if (config('laravel-xero-leave.applications_require_approval')) {
-                return !empty($this->approved_at);
+                return ! empty($this->approved_at);
             }
 
             return true;
